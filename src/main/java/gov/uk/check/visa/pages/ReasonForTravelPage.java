205e5f90cc16b20aae10f87d05690d060d4990bc
bhg9295 <bhg9295@gmail.com>
2023-07-21 23:23:15 +0100

    project done

---
+++ src/main/java/gov/uk/check/visa/pages/ReasonForTravelPage.java
@@ -0,0 +1,43 @@
+package gov.uk.check.visa.pages;
+
+import gov.uk.check.visa.utility.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class ReasonForTravelPage extends Utility {
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='response-0']")
+    WebElement reasonForVisitListTourismAndFamily;
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='response-1']")
+    WebElement reasonForVisitListWorkAcademicVisit;
+
+
+    @CacheLookup
+    @FindBy(xpath = "//button[contains(text(),'Continue')]")
+    WebElement nextStepButton;
+    @CacheLookup
+    @FindBy(xpath = "//input[@id='response-4']")
+    WebElement resonForVisitIsJoinPartnerAndFamily;
+   /* public void selectReasonForVisit(String reason){
+        selectByContainsTextFromDropDown(reasonForVisitList,reason);
+    }*/
+    public void clickOnReasonForVisitTourism()
+    {
+        clickOnElement(reasonForVisitListTourismAndFamily);
+    }
+    public void clickOnReasonForVisitAcademic()
+    {
+        clickOnElement(reasonForVisitListWorkAcademicVisit);
+    }
+    public void clickOnReasonForJoinPartnerAndFamily()
+    {
+        clickOnElement(resonForVisitIsJoinPartnerAndFamily);
+    }
+    public void clickNextStepButton()
+    {
+        clickOnElement(nextStepButton);
+    }
+
+}
